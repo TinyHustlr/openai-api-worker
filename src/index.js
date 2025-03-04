674e821565b464684ac84aca0983a202e6db6093
David O'reilly <davidworeilly@gmail.com>
2025-03-04 12:46:51 +1100

    Init commit

--- src/index.js
+++ src/index.js
@@ -8,14 +8,20 @@ const corsHeaders = {
 
 export default {
 	async fetch(request, env, ctx) {
-		if (request.method == 'OPTIONS') {
+		if (request.method === 'OPTIONS') {
 			return new Response(null, {headers: corsHeaders});
 		}
+
+		if (request.method !== POST) {
+			return new Response(JSON.stringify({ errror: `${request.method} method is not allowed`}), { status: 405, headers: corsHeaders })
+		}
+
 		const openai = new OpenAI({
-			apiKey: env.OPENAI_API_KEY
+			apiKey: env.OPENAI_API_KEY,
+			baseURL: "https://gateway.ai.cloudflare.com/v1/c638b79f5291ad3033b57af0c47d3820/stock-predictions/openai"
 		})
 		try {
-			const messages = await request.JSON()
+			const messages = await request.json()
 			const chatCompletion = await openai.chat.completions.create({
 				model: "gpt-4",
 				messages: messages
@@ -23,7 +29,7 @@ export default {
 			const response = chatCompletion.choices[0].message
 			return new Response(JSON.stringify(response), { headers: corsHeaders})
 		} catch (err) {
-			return new Response(env, { headers: corsHeaders})
+			return new Response(JSON.stringify({ error: err.message }), { status: 500, headers: corsHeaders})
 		}
 	}
 }
