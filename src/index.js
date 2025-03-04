2ee807c977dc30b08521e00793180284db27a4dd
David O'reilly <davidworeilly@gmail.com>
2025-03-04 12:29:59 +1100

    Init commit

--- src/index.js
+++ src/index.js
@@ -8,26 +8,17 @@ const corsHeaders = {
 
 export default {
 	async fetch(request, env, ctx) {
-		if (requested.method == 'OPTIONS') {
+		if (request.method == 'OPTIONS') {
 			return new Response(null, {headers: corsHeaders});
 		}
-		
 		const openai = new OpenAI({
 			apiKey: env.OPENAI_API_KEY
 		})
 		try {
+			const messages = await request.JSON()
 			const chatCompletion = await openai.chat.completions.create({
 				model: "gpt-4",
-				messages: [
-					{
-						role: "system",
-						content: "You are a stock trading super computer"
-					},
-					{
-						role: "user",
-						content: "what are the top 5 stocks to buy today"
-					}
-				]
+				messages: messages
 			})
 			const response = chatCompletion.choices[0].message
 			return new Response(JSON.stringify(response), { headers: corsHeaders})
